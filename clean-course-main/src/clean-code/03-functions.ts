29aa6b27c156d491471f0f3cb24cde54e5cf06dd
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-18 18:43:11 -0500

    03-functions refactored

--- clean-course-main/src/clean-code/03-functions.ts
+++ clean-course-main/src/clean-code/03-functions.ts
@@ -1,27 +1,22 @@
 (() => {
 
-    // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
+    function getMovieById( movieId: string ) {
         console.log({ movieId });
     }
 
-    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
-        console.log({ id });
+    function getMovieCastById( movieId: string ) {
+        console.log({ movieId });
     }
 
-    // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
+    function getActorById( ActorId: string ) {
         console.log({ ActorId });
     }
     
-    // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    function createMovie(title: string, description: string, rating: number, cast: string[] ) {
         console.log({ title, description, rating, cast });
     }
 
-    // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
+    function createActor( fullName: string, birthdate: Date ): boolean {
         
         // tarea asincrona para verificar nombre
         // ..
