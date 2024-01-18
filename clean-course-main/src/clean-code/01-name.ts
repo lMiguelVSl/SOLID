0475c6d0da6ce3662f0e6ff593ed4f932174a1dd
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-18 18:01:59 -0500

    File refactored

--- clean-course-main/src/clean-code/01-name.ts
+++ clean-course-main/src/clean-code/01-name.ts
@@ -1,19 +1,16 @@
 (() => {
 
     // Ejemplo
-    // Archivos a evaluar - files to evaluate
-    const fs = [
-        { id: 1, f: false },
-        { id: 2, f: false },
-        { id: 3, f: true },
-        { id: 4, f: false },
-        { id: 5, f: false },
-        { id: 7, f: true },
+    const filesToEvaluate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
     ]; 
     
-    // Archivos marcados para borrar - files to delete
-    const arhivos = fs.map( f => f.f );
-
+    const filesToDelete = filesToEvaluate.map( file => file.flagged );
     
     class AbstractUser { };
     class UserMixin { };
@@ -24,29 +21,19 @@
     class User { };
     interface User { };
 
+    const todayDate = new Date();
+    
+    const daysTranscurred: number = 23;
+    
+    const filesInDirectory = 33;
+    
+    const firstName = 'Fernando';
+    
+    const firstLastName = 'Herrera';
 
-    // Todo: Tarea
-        
-    // día de hoy - today
-    const ddmmyyyy = new Date();
+    const daysSinceModification = 12;
     
-    // días transcurridos - elapsed time in days
-    const d: number = 23;
-    
-    // número de archivos en un directorio - number of files in directory
-    const dir = 33;
-    
-    // primer nombre - first name
-    const nombre = 'Fernando';
-    
-    // primer apellido - last name
-    const apellido = 'Herrera';
-
-    // días desde la última modificación - days since modification
-    const dsm = 12;
-    
-    // cantidad máxima de clases por estudiante - max classes per student
-    const max = 6;
+    const maxClassesByStudent = 6;
 
 
 })();
