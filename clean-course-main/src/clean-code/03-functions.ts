b162f04fe32d1266f2d0480a246bec1276447f6f
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-18 19:00:55 -0500

    04-new function refactored and file indent

--- clean-course-main/src/clean-code/03-functions.ts
+++ clean-course-main/src/clean-code/03-functions.ts
@@ -1,63 +1,47 @@
 (() => {
 
-    function getMovieById( movieId: string ) {
+    function getMovieById(movieId: string) {
         console.log({ movieId });
     }
 
-    function getMovieCastById( movieId: string ) {
+    function getMovieCastById(movieId: string) {
         console.log({ movieId });
     }
 
-    function getActorById( ActorId: string ) {
+    function getActorById(ActorId: string) {
         console.log({ ActorId });
     }
-    
+
     interface Movie {
-        title: string; 
-        description: string; 
-        rating: number; 
+        title: string;
+        description: string;
+        rating: number;
         cast: string[];
     }
 
-    function createMovie( { title, description, rating, cast } : Movie) {
+    function createMovie({ title, description, rating, cast }: Movie) {
         console.log({ title, description, rating, cast });
     }
 
-    function checkName( fullName: string): boolean {
+    function checkName(fullName: string): boolean {
         return fullName != '';
     }
 
-    function createActor( fullName: string, birthdate: Date ): boolean {
-        
+    function createActor(fullName: string, birthdate: Date): boolean {
+
         checkName(fullName);
 
-        if ( fullName === 'fernando' ) return false;
+        if (fullName === 'fernando') return false;
 
         console.log('Crear actor', birthdate);
-        return true;        
+        return true;
 
     }
 
-    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
-        let result;
-        if ( isDead ) {
-            result = 1500;
-        } else {
-            if ( isSeparated ) {
-                result = 2500;
-            } else {
-                if ( isRetired ) {
-                    result = 3000;
-                } else {
-                    result = 4000; 
-                }
-            }
-        }
-        
-        return result;
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+        if (isDead) return 1500;
+        if (isSeparated) return 2500;
+        return (isRetired) ? 3000 : 4000;
     }
-
-    
-
 
 })();
